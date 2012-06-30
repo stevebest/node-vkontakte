647c11c3221f0f6132585e4409ca01cad7f79b49
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2012-06-30 21:22:32 +0700

    send signed requests to VK API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var http  = require('http');
-var https = require('https');
-var qs    = require('querystring');
+var http   = require('http');
+var https  = require('https');
+var qs     = require('querystring');
+var crypto = require('crypto');
 
 module.exports = function vkontakte(clientID, clientSecret) {
 
@@ -27,35 +28,80 @@ module.exports = function vkontakte(clientID, clientSecret) {
         path: '/method/' + method + '?' + qs.stringify(params)
       };
 
-      https.get(options, function (res) {
-        var responseData = '';
-        res.on('data', function (data) {
-          responseData += data.toString();
-        });
-
-        res.on('end', function() {
-          try {
-            var result = JSON.parse(responseData);
-
-            if (result.error) {
-              return callback(result.error);
-            }
-
-            return callback(null, result.response);
-          } catch (e) {
-            return callback(e);
-          }
-        });
-      }).on('error', function(e) {
-        return callback(e);
-      });
+      doRequest(https, options, callback);
     };
   }
 
   function byApp(clientID, clientSecret) {
-    return function (method, params, callback) {
-      callback(new Error('not implemented'));
+
+    // signedRequest(method, [params, [httpMethod = 'GET',]] callback)
+    return function signedRequest() {
+      var _ref, _ref1;
+      var method = arguments[0],
+          options = 2 <= arguments.length ? [].slice.call(arguments, 1) : [],
+          callback = options.pop(),
+          params = (_ref = options.shift()) != null ? _ref : {},
+          httpMethod = (_ref1 = options.shift()) != null ? _ref1 : 'GET';
+
+      params.v = '3.0';
+      params.format = 'json';
+      params.api_id = clientID;
+      params.method = method;
+      params.sig = sign(params, clientSecret);
+
+      var options = {
+        method: httpMethod,
+        host: 'api.vk.com',
+        path: '/api.php?' + qs.stringify(params)
+      };
+
+      doRequest(http, options, callback);
+    };
+
+    function sign(params, clientSecret) {
+      // Compute the signature as MD5 of request parameters joined together
+      // with a secret key, as described in VK API documentation.
+
+      var keys = Object.keys(params).sort();
+
+      var md5sum = crypto.createHash('md5');
+      for (var i = 0; i < keys.length; i++) {
+        md5sum.update('' + keys[i] + '=' + params[keys[i]]);
+      }
+      md5sum.update(clientSecret);
+
+      // Return a signature as a hex string of a MD5 hash.
+      return md5sum.digest('hex');
     }
+
+  }
+
+  function doRequest(httpClient, options, callback) {
+    var req = httpClient.request(options, function (res) {
+      var responseData = '';
+      res.on('data', function (data) {
+        responseData += data.toString();
+      });
+
+      res.on('end', function() {
+        try {
+          var result = JSON.parse(responseData);
+
+          if (result.error) {
+            return callback(result.error);
+          }
+
+          return callback(null, result.response);
+        } catch (e) {
+          return callback(e);
+        }
+      });
+    });
+
+    req.on('error', function(e) {
+      return callback(e);
+    });
+    req.end();
   }
 
 };
--- test/test.js
+++ test/test.js
@@ -16,7 +16,6 @@ describe('vkontakte', function () {
       var vk = vkontakte('no such token');
 
       it('returns error', function (done) {
-
         vk('users.get', function (err) {
           assert.throws(function () {
             assert.ifError(err);
@@ -26,8 +25,35 @@ describe('vkontakte', function () {
 
           done();
         });
-
       });
+
+    });
+
+  });
+
+  describe('vkontakte(clientID, clientSecret)', function () {
+
+    it('returns function', function () {
+      var vk = vkontakte('clientID', 'clientSecret');
+      assert.equal(typeof vk, 'function');
+    });
+
+    describe('when clientID is invalid', function () {
+
+      var vk = vkontakte('no such client ID', 'client secret');
+
+      it('returns error', function (done) {
+        vk('users.get', { uid: 1 }, function (err) {
+          assert.throws(function () {
+            assert.ifError(err);
+          }, function (err) {
+            return err.error_code === 101;
+          });
+
+          done();
+        });
+      });
+
     });
 
   });
